61860d939b76603aee309d9a26042cd898340e97
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-24 18:16:40 -0400

    got rid of all the local changes, just overriding it in the  app initializer

--- app/models/spree/digital.rb
+++ app/models/spree/digital.rb
@@ -3,20 +3,12 @@ module Spree
     belongs_to :variant
     has_many :digital_links, :dependent => :destroy
 
-    has_attached_file :attachment, path: Spree::Digital.get_path
-    validates_attachment_content_type :attachment, :content_type => %w(audio/mpeg application/x-mobipocket-ebook
-                                                                      application/epub+zip application/octet-stream
-                                                                      application/pdf application/zip image/gif
-                                                                      image/jpeg image/png)
+    has_attached_file :attachment, :path => ":rails_root/private/digitals/:id/:basename.:extension"
+    validates_attachment_content_type :attachment, :content_type => %w(audio/mpeg application/x-mobipocket-ebook application/epub+zip application/octet-stream application/pdf application/zip image/gif image/jpeg image/png)
 
     if Paperclip::Attachment.default_options[:storage] == :s3
       attachment_definitions[:attachment][:s3_permissions] = :private
       attachment_definitions[:attachment][:s3_headers] = { :content_disposition => 'attachment' }
-    end
-
-    def self.get_path
-      return '/spree/private/digitals/:id/:basename.:extension' if Paperclip::Attachment.default_options[:storage] == :s3
-      return ':rails_root/private/digitals/:id/:basename.:extension'
     end
   end
 end
