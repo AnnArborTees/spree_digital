5c51732be551cf048ae0f0ec7e67fe8dc9eb6d1e
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-24 17:59:24 -0400

    Using better path for production

--- app/models/spree/digital.rb
+++ app/models/spree/digital.rb
@@ -3,12 +3,22 @@ module Spree
     belongs_to :variant
     has_many :digital_links, :dependent => :destroy
 
-    has_attached_file :attachment, :path => ":rails_root/private/digitals/:id/:basename.:extension"
-    validates_attachment_content_type :attachment, :content_type => %w(audio/mpeg application/x-mobipocket-ebook application/epub+zip application/octet-stream application/pdf application/zip image/gif image/jpeg image/png)
+    has_attached_file :attachment, :path => :get_path
+    validates_attachment_content_type :attachment, :content_type => %w(audio/mpeg application/x-mobipocket-ebook
+                                                                      application/epub+zip application/octet-stream
+                                                                      application/pdf application/zip image/gif
+                                                                      image/jpeg image/png)
 
     if Paperclip::Attachment.default_options[:storage] == :s3
       attachment_definitions[:attachment][:s3_permissions] = :private
       attachment_definitions[:attachment][:s3_headers] = { :content_disposition => 'attachment' }
+    end
+
+    private
+
+    def get_path
+      return '/spree/private/digitals/:id/:basename.:extension' if Paperclip::Attachment.default_options[:storage] == :s3
+      return ':rails_root/private/digitals/:id/:basename.:extension'
     end
   end
 end
